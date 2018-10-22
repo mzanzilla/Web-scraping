75fde82d5472fc48287205e35d6903250c9294ce
Mike <m.mensah@hotmail.com>
2018-10-22 14:43:53 -0400

    modified the program to be able to save links to a word document

--- findlinks.py
+++ findlinks.py
@@ -5,6 +5,7 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
+out = open("links.doc", "w")
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -18,3 +19,6 @@ soup = BeautifulSoup(html, 'html.parser')
 tags = soup('a')
 for tag in tags:
     print(tag.get('href', None))
+    print(tag.get("href", None), file=out)
+    # print("something")
+out.close()
